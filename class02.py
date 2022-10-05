dcdf562131b3f0b872cb5c2f8848deac48f833bb
Jon Salhus <salhusj96@gmail.com>
2022-10-05 01:51:12 -0400

    initial commit

---
+++ class02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Script: Ops 401d5 Ops Challenge Solution 
+# Author: Jon Salhus                  
+# Date of latest revision: 10/4/2022  
+# Purpose: creates an uptime sensor tool that uses ICMP packets to evaluate if hosts on the LAN are up or down.
+
+import datetime
+import time
+import os
+
+
