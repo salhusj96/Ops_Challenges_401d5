80c9a39c2a46d42efb1fd3d1b56ed4480f09a992
Jon Salhus <salhusj96@gmail.com>
2022-11-17 02:33:46 -0500

    adds C33OC WIP

---
+++ class33.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Script: Ops 401d5 Ops Challenge Solution 33
+# Author: Jon Salhus                  
+# Date of latest revision: 11/15/2022
+# Purpose:
+
+# Import Libraries
+
+# Functions
+
+# MAIN
+
+# END
