7c028ef4824f1377fc99e0c3b36475b608ff65a5
Jonathan Salhus <108953588+salhusj96@users.noreply.github.com>
2022-12-16 12:46:20 -0500

    modifies C38OC

--- Class38.py
+++ Class38.py
@@ -1,1 +1,76 @@
+#!/usr/bin/python3
 
+# Script: Ops 401d5 Ops Challenge Solution 38
+# Author: Jon Salhus                  
+# Date of latest revision: 12/16/2022
+# Purpose: XSS Vulnerability Script, Web Security Dojo
+
+# Import libraries
+
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+
+# Declare functions
+
+# Parses into a given variable the html data from the request to the url, then returns all content in the variable and saves it 
+def get_all_forms(url):
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
+
+# Goes through all parsed data from "form" and pulls the tag information for the given attributes and saves each to a variable, then returns the results. 
+def get_form_details(form):
+    details = {}
+    action = form.attrs.get("action").lower()
+    method = form.attrs.get("method", "get").lower()
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        inputs.append({"type": input_type, "name": input_name})
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+
+# Fills out gathered information into the proper places and updates the right html file by checking for the parsed information given in previous functions, and using it in the request
+def submit_form(form_details, url, value):
+    target_url = urljoin(url, form_details["action"])
+    inputs = form_details["inputs"]
+    data = {}
+    for input in inputs:
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            data[input_name] = input_value
+
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        return requests.get(target_url, params=data)
+
+# Decodes the parsed data at time of request and gives the user a prompt with info regarding success of script and vulnerability of the web app url
+def scan_xss(url):
+    forms = get_all_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+    js_script = <script>alert('Vulnerability Detected')</script>
+    is_vulnerable = False
+    for form in forms:
+        form_details = get_form_details(form)
+        content = submit_form(form_details, url, js_script).content.decode()
+        if js_script in content:
+            print(f"[+] XSS Detected on {url}")
+            print(f"[*] Form details:")
+            pprint(form_details)
+            is_vulnerable = True
+    return is_vulnerable
+
+# Main
+
+# Prompts user for input of url, initiates the functions above and prints the subdomains of the given url. 
+if __name__ == "__main__":
+    url = input("Enter a URL to test for XSS:") 
+    print(scan_xss(url))
